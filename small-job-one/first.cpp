ced314db40d8b88cd0bc6436c5995453bd26103c
Gustavs Kavacs <gustavs.kavacs@gmail.com>
2022-10-17 22:29:13 +0300

    cleanup on ile one

--- small-job-one/first.cpp
+++ small-job-one/first.cpp
@@ -6,13 +6,13 @@ int main() {
     int num[10] = {7 , 1 , 90 , 3 , 4 , 5 , 9 , 9 , 10 , 11};
     int sum = 0;
     int avrge = 0;
+    
     for (int i = 1; i <= 10; i++) {
         if (i == 10) {
             cout << sum/i << endl;
-         } else {
+        } else {
             sum += num[i];
-         }
+        }
     }
-
 return 0;
 }
