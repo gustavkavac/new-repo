d2db95764a9087f4c1a15112cea6127da92387a8
Gustavs Kavacs <gustavs.kavacs@gmail.com>
2022-10-18 23:01:31 +0300

    atkapes un noformejums

--- small-job-one/fifth.cpp
+++ small-job-one/fifth.cpp
@@ -6,14 +6,15 @@ int main() {
     char output[1024];
     int output_size = 0;
     cout << "Ierakstiet simbolu virkni: ";
+    
     cin.getline(input, 1024);
     cout << '\n';
-        for(int i = 0; i < 1024; i++) {
-            if (isdigit(input[i]) == 0) {
-                output[output_size] = input[i];
-                output_size++;
-            }
+    for(int i = 0; i < 1024; i++) {
+        if (isdigit(input[i]) == 0) {
+            output[output_size] = input[i];
+            output_size++;
         }
+    }
     cout << output << endl;    
     return 0;
 }
