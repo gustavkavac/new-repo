e1abe7f5cf9cb175c68d90797ae30755882c9605
Gustavs Kavacs <gustavs.kavacs@gmail.com>
2022-10-18 22:41:11 +0300

    atstarpes masiva un tab pie returna

--- small-job-one/second.cpp
+++ small-job-one/second.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    float num[10] = {-110.4324 , -1 , -90 , -112.3453 , 1478 , -5 , -9 , -9 , -10 , 123456git};
+    float num[10] = {-110.4324,-1,-90,-112.3453,147,-5,-9,-9,-10,12345};
     float num_max = num[0];
     
     for (int i = 1; i < 10; ++i) {
