9d4ebf8c9162fe6bb6cd4fa837cb1f3b689f8c91
Gustavs Kavacs <gustavs.kavacs@gmail.com>
2022-10-19 08:44:04 +0300

    uztaisiju ar string char vieta

--- small-job-one/fourth.cpp
+++ small-job-one/fourth.cpp
@@ -2,15 +2,16 @@
 using namespace std;
 
 int main() {
-    char input[1024];
+    string input;
     cout << "Ierakstiet dazhadus burtus: ";
-    cin.getline(input, 1024);
+    getline(cin, input);
+
     int a_counter = 0;
-        for(int i = 0; i < 1024; i++) {
+        for(int i = 0; i < input.size(); i++) {
             if (input[i] == 'a') {
                 a_counter++;
             }
         }
     cout << "Jusu virkne bija " << a_counter << " burti 'a'." << endl;
-   return 0;
+    return 0;
 }
