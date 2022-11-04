a81333165efbffc9daa1d9f9451aa7d586dcc4ad
Gustavs Kavacs <gustavs.kavacs@gmail.com>
2022-11-04 19:19:33 +0200

    adding fourth

--- small-job-four/fourth.cpp
+++ small-job-four/fourth.cpp
@@ -1,6 +1,20 @@
 #include <iostream> 
+#include <array>
 using namespace std;
 
+void print_reverse(array<int, 10> input);
+
 int main(){
-    
+    array<int, 10> nmbr{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+    print_reverse(nmbr);
+
+}
+
+void print_reverse(array<int, 10> input){
+    int *pointNum;
+    for(int i = 9; i > -1; --i){
+    pointNum = &input[i];
+    cout << *pointNum << ' ';
+    }
 }
