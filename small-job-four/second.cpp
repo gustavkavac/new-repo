e592aa86637aa38fb7b571fefc4d96e9380164fe
Gustavs Kavacs <gustavs.kavacs@gmail.com>
2022-11-07 00:02:23 +0200

    laikam salaboju pirmos tris

--- small-job-four/second.cpp
+++ small-job-four/second.cpp
@@ -4,18 +4,27 @@ using namespace std;
 int main(){
     int num_mass[10] = {4, 2, 8, 3, 5, 6, 7, 9, 11, 15};
     int *pointNum;
-    int input;
+    int *input;
     bool itis = false;
-    int i = 0;
+    int *i;
+
+    input = new int;
+    i = new int;
+    *i = 0;
 
     cout << "Ievadiet skailti, lai parbauditu vai tas ir skaitlju rindaa: ";
-    cin >> input;
-    pointNum = &input; 
+    cin >> *input;
+    cin.ignore(1000,'\n');
+    cin.clear();
 
-    for(i = 0; i < 10; i++){
-        if(*pointNum == num_mass[i]){
+    while(itis == false){
+        if(*input == num_mass[*i]){
             itis = true;
             break;
+        } else if(*i == 10){
+            break;
+        } else {
+            *i = *i + 1;
         }
     }
     
