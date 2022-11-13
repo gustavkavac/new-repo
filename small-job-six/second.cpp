ebfb6c17ec26d662119322da1481e0d52cc43991
gustavkavac <gustavs.kavacs@gmail.com>
2022-11-13 16:20:27 +0200

    otra sachinita

--- small-job-six/second.cpp
+++ small-job-six/second.cpp
@@ -2,18 +2,40 @@
 #include <fstream>
 using namespace std;
 
+bool isPrime(int x);
+
 int main() {
     int numArr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     for (int i = 0; i < (sizeof(numArr) / sizeof(numArr[0])); i++) {
-        if (numArr[i] % 2 == 0) {
-            ofstream even("even.txt", ios::app);
+        if ((numArr[i] % 2 == 1) && (isPrime(numArr[i]) == 0)) {
+            ofstream even("file2.txt", ios::app);
             even << numArr[i] << endl;
             even.close();
-        } else if (numArr[i] % 2 == 1) {
-            ofstream unEven("uneven.txt", ios::app);
+        } else if ((numArr[i] % 2 == 0) && (isPrime(numArr[i]) == 0)) {
+            ofstream unEven("file1.txt", ios::app);
             unEven << numArr[i] << endl;
             unEven.close();
+        } else if (isPrime(numArr[i])) {
+            ofstream prime("file3.txt", ios::app);
+            prime << numArr[i] << endl;
+            prime.close();           
         }
     }
     return 0;
+}
+
+bool isPrime(int x) {
+    bool isPrime = true;
+
+    if (x == 0 || x == 1) {
+        isPrime = false;
+    }
+
+    for (int i = 2; i <= x / 2; i++) {
+        if (x % i == 0) {
+            isPrime = false;
+        }
+    }
+
+    return isPrime;
 }
